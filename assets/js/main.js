ed3346a26b419c63550bd494c521bec045bdb817
tinykitelab <dakota@tinykitelab.com>
2018-03-31 14:55:26 -0500

    Added simple obfuscation

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,3 +31,22 @@ tl.timeScale(0.75)
   .add("round6")
   .to (sqWindowGroup1, 2, {autoAlpha: 0, ease:Power1.easeOut}, "round6+=3")
   
+var Contact = {};
+
+Contact.deobfuscateLink = function(element) {
+    var absolutePath   = element.href,
+        pathSegments   = absolutePath.split('/'),
+        obfuscatedLink = pathSegments[pathSegments.length - 1],
+        unreversedLink = obfuscatedLink.split('').reverse().join(''),
+        deobfuscation  = decodeURIComponent(unreversedLink);
+    return deobfuscation;
+}
+
+Contact.patchButtons = function(klass) {
+    var elements = document.getElementsByClassName(klass);
+    for (i = 0; i < elements.length; i++) {
+        elements[i].href = Contact.deobfuscateLink(elements[i]);
+    }
+}
+
+Contact.patchButtons('email');
